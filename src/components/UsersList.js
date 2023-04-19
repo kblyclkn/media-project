1435d4769f2fe1f0b691654a75e346efcc03e0fa
kblyclkn <105425261+kblyclkn@users.noreply.github.com>
2023-04-19 13:02:38 +0300

    addUser thunks completed

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store";
+import { fetchUsers, addUser } from "../store";
+import Button from "./Button";
+import Skeleton from "./Skeleton";
+
 function UsersList() {
   const dispatch = useDispatch();
   const { isLoading, data, error } = useSelector((state) => {
@@ -10,15 +13,39 @@ function UsersList() {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  if(isLoading) {
-    return <div>Loading...</div>
+ const handleUserAdd = () => {
+  dispatch(addUser());
+ }
+
+  if (isLoading) {
+    return <Skeleton times={6} className="h-10 w-full" />;
   }
 
-  if(error){
-    return <div>Error fetching data..</div>
+  if (error) {
+    return <div>Error fetching data..</div>;
   }
 
-  return <div>{data.length}</div>;
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={handleUserAdd}>
+          + Add User
+        </Button>
+      </div>
+      {renderedUsers}
+    </div>
+  );
 }
 
 export default UsersList;
